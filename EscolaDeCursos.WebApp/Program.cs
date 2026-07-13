2b1816d1bf8516b84ba8ee299ae58dc48f67315f
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-07-13 14:39:45 -0300

    Adiciona health check do DbContext

--- EscolaDeCursos.WebApp/Program.cs
+++ EscolaDeCursos.WebApp/Program.cs
@@ -1,14 +1,23 @@
 using EscolaDeCursos.Aplicacao;
 using EscolaDeCursos.Infra;
+using EscolaDeCursos.Infra.Compartilhado.Orm;
 using EscolaDeCursos.WebApp.Compartilhado;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Configuração do container de injeção de dependência
-
 builder.Services.AddInfraRepositories(builder.Configuration, builder.Logging);
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddPresentationConfig(builder.Configuration);
+
+// Configura health checks do banco de dados
+builder.Services.AddHealthChecks()
+    .AddDbContextCheck<EscolaDeCursosDbContext>(
+        name: "database_check",
+        failureStatus: HealthStatus.Unhealthy,
+        tags: ["ready"]
+    );
 
 var app = builder.Build();
 
