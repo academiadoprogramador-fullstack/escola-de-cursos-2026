3fec2ba2072378e93d9cb9ae179fc892dd92bded
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-07-16 14:29:16 -0300

    Adiciona AspNetCore.Identity e cria Migrations

--- EscolaDeCursos.Infra/Compartilhado/Orm/EscolaDeCursosDbContext.cs
+++ EscolaDeCursos.Infra/Compartilhado/Orm/EscolaDeCursosDbContext.cs
@@ -2,15 +2,19 @@ using System.Reflection;
 using EscolaDeCursos.Dominio.Modulos.ModuloAluno;
 using EscolaDeCursos.Dominio.Modulos.ModuloCategoria;
 using EscolaDeCursos.Dominio.Modulos.ModuloCurso;
+using EscolaDeCursos.Dominio.Modulos.ModuloInstituicao;
 using EscolaDeCursos.Dominio.Modulos.ModuloInstrutor;
 using EscolaDeCursos.Dominio.Modulos.ModuloMatricula;
 using EscolaDeCursos.Dominio.Modulos.ModuloTurma;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EscolaDeCursos.Infra.Compartilhado.Orm;
 
 public sealed class EscolaDeCursosDbContext(
-    DbContextOptions<EscolaDeCursosDbContext> options) : DbContext(options)
+    DbContextOptions<EscolaDeCursosDbContext> options
+) : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>(options)
 {
     public DbSet<Categoria> Categorias => Set<Categoria>();
     public DbSet<Curso> Cursos => Set<Curso>();
@@ -19,9 +23,12 @@ public sealed class EscolaDeCursosDbContext(
     public DbSet<Aluno> Alunos => Set<Aluno>();
     public DbSet<Turma> Turmas => Set<Turma>();
     public DbSet<Matricula> Matriculas => Set<Matricula>();
+    public DbSet<Instituicao> Instituicoes => Set<Instituicao>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         Assembly assembly = typeof(EscolaDeCursosDbContext).Assembly;
 
         modelBuilder.ApplyConfigurationsFromAssembly(assembly);
--- EscolaDeCursos.Infra/InjecaoDependencia.cs
+++ EscolaDeCursos.Infra/InjecaoDependencia.cs
@@ -12,6 +12,7 @@ using EscolaDeCursos.Infra.Modulos.ModuloCurso;
 using EscolaDeCursos.Infra.Modulos.ModuloInstrutor;
 using EscolaDeCursos.Infra.Modulos.ModuloMatricula;
 using EscolaDeCursos.Infra.Modulos.ModuloTurma;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -52,6 +53,25 @@ public static class InjecaoDependencia
                 opt.EnableRetryOnFailure(3);
             });
         });
+
+        // Configuração do Usuário no Identity
+        services.AddIdentityCore<IdentityUser<Guid>>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+            options.SignIn.RequireConfirmedEmail = false;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireDigit = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.AllowedForNewUsers = true;
+        })
+        .AddRoles<IdentityRole<Guid>>() // Configuração de Cargos/Papéis no Identity
+        .AddEntityFrameworkStores<EscolaDeCursosDbContext>() // Integração com EntityFramework
+        .AddSignInManager()
+        .AddDefaultTokenProviders();
 
         // Injeta os repositórios
         services.AddScoped<IRepositorioCategoria, RepositorioCategoriaEmOrm>();
